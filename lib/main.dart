56ca2658d842c4da98a5ffc9fa08d9aeb7b50511
DYSTO_pyy <bcd.saltel@gmail.com>
2024-04-15 13:03:24 +0200

    feat: image & text added

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   runApp(const MainApp());
 }
 
@@ -13,20 +14,32 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        
         body: Center(
-          child: Container( 
-            decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xFFD00000),
-                Color(0xFFFFBA08),
-              ],
-            )
+          child: SizedBox.expand(
+            child: Container(
+                
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("LEMAITRE Maxime, MENU Thomas, SALTEL Baptiste", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 12),),
+                        Image(
+                            image: AssetImage('assets/akari_icon_basic.png')),
+                        Text("Joue des parties,\nGagne des pièces,\nCustomise ton jeu !", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 20),)
+                      ],
+                    ),
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                      Color(0xFFD00000),
+                      Color(0xFFFFBA08),
+                  ],
+                ))),
           ),
-          )
         ),
       ),
     );
