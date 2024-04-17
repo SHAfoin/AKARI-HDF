79cacc2bcc8e1b7334667b36a0634c584500699c
DYSTO_pyy <bcd.saltel@gmail.com>
2024-04-17 18:24:10 +0200

    feat: ajout du package GoogleFont

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,30 +16,45 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
       home: Scaffold(
-        
         body: Center(
           child: SizedBox.expand(
             child: Container(
-                
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("LEMAITRE Maxime, MENU Thomas, SALTEL Baptiste", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 12),),
-                        Image(
-                            image: AssetImage('assets/akari_icon_basic.png')),
-                        Text("Joue des parties,\nGagne des pièces,\nCustomise ton jeu !", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 20),)
-                      ],
-                    ),
                 decoration: const BoxDecoration(
                     gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                      Color(0xFFD00000),
-                      Color(0xFFFFBA08),
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xFFD00000),
+                    Color(0xFFFFBA08),
                   ],
-                ))),
+                )),
+                child: Column(
+                  
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    
+                    Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(color: Colors.black),
+                  ),
+                    Text(
+                      "LEMAITRE Maxime, MENU Thomas, SALTEL Baptiste",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.concertOne(color: Colors.white, fontSize: 12 ),
+                    ),
+                    Image(image: AssetImage('assets/akari_icon_basic.png')),
+                    Text(
+                      "Joue des parties,\nGagne des pièces,\nCustomise ton jeu !",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.concertOne(color: Colors.white, fontSize: 20 ),
+                    )
+                  ],
+                )),
           ),
         ),
       ),
