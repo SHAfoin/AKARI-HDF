ff2f554bcf6117726fadcf611cf5b2cdd8fd9a4b
maxlem24 <lemaitre.maxime2004@gmail.com>
2024-05-05 17:33:13 +0200

    Solveur Fini, à integrer avec le reste maintenant

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:akari_project/models.dart';
 import 'package:akari_project/solution.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   final List<List<int>> matrice1 = [[0,12,0,10,0,0,0],[0,0,0,11,0,0,10],[0,0,0,0,0,0,0],[11,10,0,0,0,6,6],[0,0,0,0,0,0,0],[11,0,0,10,0,0,0],[0,0,0,6,0,12,0]];
   final List<List<int>> matrice2 = [[0,0,0,0,6,0,0],[0,0,13,0,0,0,0],[12,0,0,14,0,6,0],[0,0,6,0,6,0,0],[0,6,0,6,0,0,11],[0,0,0,0,13,0,0],[0,0,10,0,0,0,0]];
-  var solv = TrivialSolver(matrice2);
-  solv.afficherMat();
-  solv.solve();
-  solv.afficherMat();
+  var solv = Solveur();
+  Grille puzzle = Grille(matrice1.length, matrice1);
+  List<Grille> solutions = solv.backtrackSolveur(puzzle);
+  for (var grille in solutions) {
+    grille.afficherMat();
+  }
   // runApp(const MainApp());
 }
 
--- lib/solution.dart
+++ lib/solution.dart
@@ -1,197 +1,186 @@
-import 'package:akari_project/resolveur.dart';
+import 'package:akari_project/models.dart';
 
-enum Cases {
-  caseNonEclaire(0),
-  mur(6),
-  caseEclaire(1),
-  ampoule(5),
-  ampouleRouge(9),
-  point(99),
-  zeroCell(10),
-  oneCell(11),
-  twoCell(12),
-  threeCell(13),
-  fourCell(14);
-
-  final int value;
-  const Cases(this.value);
-}
-
-class TrivialSolver {
-  final List<List<int>> matrice;
-  final List<List<bool>> possibilite;
-
-  afficherMat() {
-    for (var i = 0; i < matrice.length; i++) {
-      print(matrice[i]);
-    }
-    for (var i = 0; i < possibilite.length; i++) {
-      print(possibilite[i]);
-    }
-  }
-
-  TrivialSolver(this.matrice)
-      : possibilite = List<List<bool>>.generate(
-            matrice.length,
-            (i) => List<bool>.generate(matrice.length,
-                (j) => matrice[i][j] == Cases.caseNonEclaire.value));
-
-  zerosCell() {
-    for (var i = 0; i < matrice.length; i++) {
-      for (var j = 0; j < matrice.length; j++) {
-        if (matrice[i][j] == Cases.zeroCell.value) {
-          if (i - 1 >= 0 && matrice[i - 1][j] == Cases.caseNonEclaire.value) {
-            possibilite[i - 1][j] = false;
+class Solveur {
+  void zerosCell(Grille puzzle) {
+    for (var i = 0; i < puzzle.length; i++) {
+      for (var j = 0; j < puzzle.length; j++) {
+        if (puzzle.isCase(i, j, Cases.zeroCell)) {
+          if (i - 1 >= 0 && puzzle.isCase(i - 1, j, Cases.nonEclaire)) {
+            puzzle.removeCandidate(i - 1, j);
           }
-          if (i + 1 < matrice.length &&
-              matrice[i + 1][j] == Cases.caseNonEclaire.value) {
-            possibilite[i + 1][j] = false;
+          if (i + 1 < puzzle.length &&
+              puzzle.isCase(i + 1, j, Cases.nonEclaire)) {
+            puzzle.removeCandidate(i + 1, j);
           }
-          if (j - 1 >= 0 && matrice[i][j - 1] == Cases.caseNonEclaire.value) {
-            possibilite[i][j - 1] = false;
+          if (j - 1 >= 0 && puzzle.isCase(i, j - 1, Cases.nonEclaire)) {
+            puzzle.removeCandidate(i, j - 1);
           }
-          if (j + 1 < matrice.length &&
-              matrice[i][j + 1] == Cases.caseNonEclaire.value) {
-            possibilite[i][j + 1] = false;
+          if (j + 1 < puzzle.length &&
+              puzzle.isCase(i, j + 1, Cases.nonEclaire)) {
+            puzzle.removeCandidate(i, j + 1);
           }
         }
       }
     }
   }
 
-  int nbVoisinsLibre(int i, int j) {
-    int total = 0;
-    if (i - 1 >= 0 && possibilite[i - 1][j]) {
-      total++;
-    }
-    if (i + 1 < possibilite.length && possibilite[i + 1][j]) {
-      total++;
-    }
-    if (j - 1 >= 0 && possibilite[i][j - 1]) {
-      total++;
-    }
-    if (j + 1 < possibilite.length && possibilite[i][j + 1]) {
-      total++;
-    }
-    return total;
-  }
-
-  int nbVoisinsAmpoule(int i, int j) {
-    int total = 0;
-    if (i - 1 >= 0 && matrice[i - 1][j] == Cases.ampoule.value) {
-      total++;
-    }
-    if (i + 1 < matrice.length && matrice[i + 1][j] == Cases.ampoule.value) {
-      total++;
-    }
-    if (j - 1 >= 0 && matrice[i][j - 1] == Cases.ampoule.value) {
-      total++;
-    }
-    if (j + 1 < matrice.length && matrice[i][j + 1] == Cases.ampoule.value) {
-      total++;
-    }
-    return total;
-  }
-
-  (int, int) nCellwithNVoisins() {
-    for (var i = 0; i < matrice.length; i++) {
-      for (var j = 0; j < matrice.length; j++) {
-        switch (matrice[i][j]) {
-          case 11:
-            if ((nbVoisinsLibre(i, j) + nbVoisinsAmpoule(i, j)) == 1 &&
-                nbVoisinsLibre(i, j) != 0) {
+  (int, int) nCellwithNVoisins(Grille puzzle) {
+    for (var i = 0; i < puzzle.length; i++) {
+      for (var j = 0; j < puzzle.length; j++) {
+        switch (puzzle.get(i, j)) {
+          case Cases.oneCell:
+            if ((puzzle.nbVoisinsLibre(i, j) + puzzle.nbVoisinsAmpoule(i, j)) ==
+                    1 &&
+                puzzle.nbVoisinsLibre(i, j) != 0) {
               return (i, j);
             }
             break;
-          case 12:
-            if ((nbVoisinsLibre(i, j) + nbVoisinsAmpoule(i, j)) == 2 &&
-                nbVoisinsLibre(i, j) != 0) {
+          case Cases.twoCell:
+            if ((puzzle.nbVoisinsLibre(i, j) + puzzle.nbVoisinsAmpoule(i, j)) ==
+                    2 &&
+                puzzle.nbVoisinsLibre(i, j) != 0) {
               return (i, j);
             }
             break;
-          case 13:
-            if ((nbVoisinsLibre(i, j) + nbVoisinsAmpoule(i, j)) == 3 &&
-                nbVoisinsLibre(i, j) != 0) {
+          case Cases.threeCell:
+            if ((puzzle.nbVoisinsLibre(i, j) + puzzle.nbVoisinsAmpoule(i, j)) ==
+                    3 &&
+                puzzle.nbVoisinsLibre(i, j) != 0) {
               return (i, j);
             }
             break;
-          case 14:
-            if ((nbVoisinsLibre(i, j) + nbVoisinsAmpoule(i, j)) == 4 &&
-                nbVoisinsLibre(i, j) != 0) {
+          case Cases.fourCell:
+            if ((puzzle.nbVoisinsLibre(i, j) + puzzle.nbVoisinsAmpoule(i, j)) ==
+                    4 &&
+                puzzle.nbVoisinsLibre(i, j) != 0) {
               return (i, j);
             }
             break;
           default:
+            break;
         }
       }
     }
     return (-1, -1);
   }
 
-  poserAmpoule(int iAmpoule, int jAmpoule) {
-    matrice[iAmpoule][jAmpoule] = Cases.ampoule.value;
-    possibilite[iAmpoule][jAmpoule] = false;
-    var i = iAmpoule;
-    var j = jAmpoule;
-    i--;
-    while (i >= 0 &&
-        (matrice[i][j] == Cases.caseNonEclaire.value ||
-            matrice[i][j] == Cases.caseEclaire.value)) {
-      matrice[i][j] = Cases.caseEclaire.value;
-      possibilite[i][j] = false;
-      i--;
-    }
-    i = iAmpoule;
-    i++;
-    while (i < matrice.length &&
-        (matrice[i][j] == Cases.caseNonEclaire.value ||
-            matrice[i][j] == Cases.caseEclaire.value)) {
-      matrice[i][j] = Cases.caseEclaire.value;
-      possibilite[i][j] = false;
-      i++;
-    }
-    i = iAmpoule;
-    j--;
-    while (j >= 0 &&
-        (matrice[i][j] == Cases.caseNonEclaire.value ||
-            matrice[i][j] == Cases.caseEclaire.value)) {
-      matrice[i][j] = Cases.caseEclaire.value;
-      possibilite[i][j] = false;
-      j--;
-    }
-    j = jAmpoule;
-    j++;
-    while (j < matrice.length &&
-        (matrice[i][j] == Cases.caseNonEclaire.value ||
-            matrice[i][j] == Cases.caseEclaire.value)) {
-      matrice[i][j] = Cases.caseEclaire.value;
-      possibilite[i][j] = false;
-      j++;
-    }
-    j = jAmpoule;
-  }
-
-  nCell(int i, int j) {
-    if (i - 1 >= 0 && possibilite[i - 1][j]) {
-      poserAmpoule(i - 1, j);
-    }
-    if (i + 1 < possibilite.length && possibilite[i + 1][j]) {
-      poserAmpoule(i + 1, j);
-    }
-    if (j - 1 >= 0 && possibilite[i][j - 1]) {
-      poserAmpoule(i, j - 1);
-    }
-    if (j + 1 < possibilite.length && possibilite[i][j + 1]) {
-      poserAmpoule(i, j + 1);
-    }
-  }
-
-  solve() {
-    zerosCell();
-    var (i, j) = nCellwithNVoisins();
+  void trivialSolveur(Grille puzzle) {
+    zerosCell(puzzle);
+    var (int i, int j) = nCellwithNVoisins(puzzle);
     while (i != -1 && j != -1) {
-      nCell(i, j);
-      (i, j) = nCellwithNVoisins();
+      puzzle.ampouleNCell(i, j);
+      (i, j) = nCellwithNVoisins(puzzle);
     }
+  }
+
+  bool checkWallsCompleteness(Grille puzzle) {
+    for (var i = 0; i < puzzle.length; i++) {
+      for (var j = 0; j < puzzle.length; j++) {
+        switch (puzzle.get(i, j)) {
+          case Cases.oneCell:
+            if (puzzle.nbVoisinsAmpoule(i, j) != 1) {
+              return false;
+            }
+            break;
+          case Cases.twoCell:
+            if (puzzle.nbVoisinsAmpoule(i, j) != 2) {
+              return false;
+            }
+            break;
+          case Cases.threeCell:
+            if (puzzle.nbVoisinsAmpoule(i, j) != 3) {
+              return false;
+            }
+            break;
+          case Cases.fourCell:
+            if (puzzle.nbVoisinsAmpoule(i, j) != 4) {
+              return false;
+            }
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    return true;
+  }
+
+  bool isSolved(Grille puzzle) {
+    if (!checkWallsCompleteness(puzzle)) {
+      return false;
+    }
+    for (var i = 0; i < puzzle.length; i++) {
+      for (var j = 0; j < puzzle.length; j++) {
+        if (puzzle.isCase(i, j, Cases.nonEclaire)) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  bool checkWallsFeasability(Grille puzzle) {
+    for (var i = 0; i < puzzle.length; i++) {
+      for (var j = 0; j < puzzle.length; j++) {
+        switch (puzzle.get(i, j)) {
+          case Cases.oneCell:
+            if (puzzle.nbVoisinsAmpoule(i, j) > 1 &&
+                (puzzle.nbVoisinsLibre(i, j) + puzzle.nbVoisinsAmpoule(i, j)) <
+                    1) {
+              return false;
+            }
+            break;
+          case Cases.twoCell:
+            if (puzzle.nbVoisinsAmpoule(i, j) > 2 &&
+                (puzzle.nbVoisinsLibre(i, j) + puzzle.nbVoisinsAmpoule(i, j)) <
+                    2) {
+              return false;
+            }
+            break;
+          case Cases.threeCell:
+            if (puzzle.nbVoisinsAmpoule(i, j) > 3 &&
+                (puzzle.nbVoisinsLibre(i, j) + puzzle.nbVoisinsAmpoule(i, j)) <
+                    3) {
+              return false;
+            }
+            break;
+          case Cases.fourCell:
+            if (puzzle.nbVoisinsAmpoule(i, j) > 4 &&
+                (puzzle.nbVoisinsLibre(i, j) + puzzle.nbVoisinsAmpoule(i, j)) <
+                    4) {
+              return false;
+            }
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    return true;
+  }
+
+  void backtracking(Grille puzzle, List<Grille> solutions) {
+    if (isSolved(puzzle)) {
+      solutions.add(puzzle);
+      return;
+    }
+    var (int i, int j) = puzzle.nextCandidate();
+    if (!checkWallsFeasability(puzzle) || (i == -1 || j == -1)) {
+      return;
+    }
+    Grille copyAmpoule = Grille.copy(puzzle);
+    copyAmpoule.poserAmpoule(i, j);
+    backtracking(copyAmpoule, solutions);
+    Grille copySansAmpoule = Grille.copy(puzzle);
+    copySansAmpoule.removeCandidate(i,j);
+    backtracking(copySansAmpoule, solutions);
+  }
+
+  List<Grille> backtrackSolveur(Grille puzzle) {
+    Grille copy = Grille.copy(puzzle);
+    trivialSolveur(copy);
+    List<Grille> solutions = List<Grille>.empty(growable: true);
+    backtracking(copy, solutions);
+    return solutions;
   }
 }
