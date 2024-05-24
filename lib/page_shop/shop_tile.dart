5abd88416c99947efde0806c514392f2d7ca94cd
DYSTO_pyy <bcd.saltel@gmail.com>
2024-05-24 17:59:45 +0200

    feat: ajout de l'achat des item dans la boutique au clic

--- lib/page_shop/shop_tile.dart
+++ lib/page_shop/shop_tile.dart
@@ -1,11 +1,17 @@
 import 'package:akari_project/page_shop/shop_item.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
-class ShopTile extends StatelessWidget {
+class ShopTile extends StatefulWidget {
   final ShopItem item;
 
   const ShopTile({super.key, required this.item});
 
+  @override
+  State<ShopTile> createState() => _ShopTileState();
+}
+
+class _ShopTileState extends State<ShopTile> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,40 +27,75 @@ class ShopTile extends StatelessWidget {
                 decoration:
                     BoxDecoration(borderRadius: BorderRadius.circular(20)),
                 child: Image(
-                  image: AssetImage(item.pathToImage),
+                  image: AssetImage(widget.item.pathToImage),
                 )),
           ),
           Text(
-            item.name,
+            widget.item.name,
             style: const TextStyle(color: Colors.black, fontSize: 20),
           ),
-          Container(
-            height: 45,
-            margin: const EdgeInsets.all(10),
-            width: double.infinity,
-            decoration: BoxDecoration(
-                color: const Color(0xFF370617), borderRadius: BorderRadius.circular(20)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: Text(
-                    item.isBought ? "Acheté !" : item.price.toString(),
-                    style: const TextStyle(color: Colors.white, fontSize: 20),
+          GestureDetector(
+            onTap: () {
+              var obtenu = widget.item.isBought;
+              var coins = Hive.box('userBox').get('coins');
+              if (!obtenu) {
+                showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    title: Text(widget.item.name),
+                    content: const Text("Confirmer l'achat ?"),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () => Navigator.pop(context, 'Cancel'),
+                        child: const Text('Non'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            Hive.box('userBox')
+                                .put('coins', coins - widget.item.price);
+                            widget.item.isBought = true;
+                            Navigator.pop(context, 'OK');
+                          });
+                        },
+                        child: const Text('Oui'),
+                      ),
+                    ],
                   ),
-                ),
-                Visibility(
-                  visible: !item.isBought,
-                  child: const Padding(
-                    padding:
-                        EdgeInsets.only(top: 8.0, left: 8.0, bottom: 8.0),
-                    child: Image(
-                      image: AssetImage('assets/images/coin.png'),
+                );
+              }
+            },
+            child: Container(
+              height: 45,
+              margin: const EdgeInsets.all(10),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: const Color(0xFF370617),
+                  borderRadius: BorderRadius.circular(20)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      widget.item.isBought
+                          ? "Acheté !"
+                          : widget.item.price.toString(),
+                      style: const TextStyle(color: Colors.white, fontSize: 20),
                     ),
                   ),
-                )
-              ],
+                  Visibility(
+                    visible: !widget.item.isBought,
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.only(top: 8.0, left: 8.0, bottom: 8.0),
+                      child: Image(
+                        image: AssetImage('assets/images/coin.png'),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           )
         ],
